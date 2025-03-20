b3501dc246626587ad1c85be24bc9026220bff2d
Nitin <nitin.misra84@gmail.com>
2025-03-19 17:52:01 -0700

    mobile view for nav component complete

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,13 +1,22 @@
-import "./searchBar.css"
+import { useState } from "react";
+import "./searchBar.css";
 
 const SearchBar = ({ placeholder, icon }) => {
-  return (
-    <div className="input-wrapper">
-      <input placeholder={placeholder} />
-      <span className="icon">{icon}</span>
-    </div>
+  const [isVisible, setIsVisible] = useState(false);
 
-  )
-}
+  const toggleSearch = () => {
+    setIsVisible(!isVisible);
+  };
+
+  return (
+    <div className={`input-wrapper ${isVisible ? "expanded" : ""}`}>
+      <input 
+        placeholder={placeholder} 
+        className={isVisible ? "visible" : "hidden"} 
+      />
+      <span className="icon" onClick={toggleSearch}>{icon}</span>
+    </div>
+  );
+};
 
 export default SearchBar;
